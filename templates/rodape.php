95b1f82de4ac9cccf2bfaea9cc00eb8b990e31bf
lucasperrotta <perrotta.lucas@gmail.com>
2015-05-25 22:06:47 -0300

    Alterar Template Rodape

---
+++ templates/rodape.php
@@ -0,0 +1,15 @@
+	</div><!--FECHA CONTENTSIDEBAR-->
+
+			</div><!--FECHA FUNDO CONTENT-->
+
+			<div id="footer">
+
+				<p align="center">CAT SKATE SHOP BITCHIES!</p>
+
+			</div><!--FECHA FOOTER-->
+
+		</div><!--FECHA WRAPPER-->
+
+	</body>
+
+</html>
