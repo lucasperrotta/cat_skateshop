dca1f924440c768675d41aa2d73cf8d6fe6ff43a
Lucas Perrotta Barbosa <perrotta.lucas@gmail.com>
2015-05-30 00:40:40 -0300

    Linkar Crud No Cabecario

--- templates/cabecario.php
+++ templates/cabecario.php
@@ -15,5 +15,13 @@
 				<div id="divlogo">
 					<img src="img/logozica1.png" id="logo">
 				</div><!--FECHA LOGO-->
+				<div id="login">
+					<ul>
+						<li> <a href="cadastra_cliente.php">Cadastro de Cliente</a> </li>
+						<li> <a href="cadastra_produto.php">Cadastro de produto</a> </li>
+						<li> <a href="consulta_clientes.php">Consulta de Clientes</a> </li>
+						<li> <a href="consulta_produtos.php">Consulta de Produtos</a> </li>
+					</ul>
+				</div>	
 				
 			</div><!--FECHA HEADER-->
