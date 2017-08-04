074cb378f44e20e70b1d686e9dab87f18b193e59
Lucas Perrotta Barbosa <perrotta.lucas@gmail.com>
2017-08-04 15:25:23 -0300

    Mudando conexão para PDO

--- conexao/inicio_conexao.php
+++ conexao/inicio_conexao.php
@@ -1,6 +1,26 @@
 <?php
-$con = mysql_connect("localhost", "root", "")
-	or die ("A conexão com o servidor não foi executada com sucesso!");
-$db = mysql_select_db("cat_skateshop", $con)
-	or die ("Não foi possível selecionar o banco de dados!");
+function conectar ()
+{
+    $host = "localhost";
+    $dbn = "cat_skateshop"; //NOME DO BANCO
+    $user = "root"; //USUARIO DO BANCO
+    $pass = ""; //SENHA DO USUARIO DO  BANCO
+
+    $conexao = new PDO("mysql:dbname=$dbn; host=$host", $user, $pass);
+
+    return $conexao;
+}
+
+conectar();
+
+if (!conectar()) {
+    echo "Erro ao tentar conectar com o banco.";
+}
+
+/*
+  LEMBRETE EM TODAS AS PAGINAS QUE HOUVER CONEXÃO COM O BANCO DE DADOS USAR:
+  require_once('conexao/conexao.php');
+  SE FOR PARTE ADMINISTRATIVA LEMBAR DE COLOCAR UM NÍVEL ACIMA:
+  require_once('../conexao/conexao.php');
+ */
 ?>
